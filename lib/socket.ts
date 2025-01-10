b4a07a15019ae5773c52d5b9057cbf79d0ac7f91
Shanu Kumar <118092340+ShanuRocky@users.noreply.github.com>
2025-01-11 03:54:08 +0530

    change

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.URL || 'http://localhost:5000');
+const socket = io(process.env.NEXT_PUBLIC_API_URL);
 
 export const initializeSocket = (userId: string) => {
   socket.emit('join_room', userId);
